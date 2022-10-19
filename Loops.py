5d6db6fe24d3ad66b314a645f05a4fc65fe5ae36
ShrutiPethani123 <shrutipethani.royal@gmail.com>
2022-10-19 16:54:24 +0530

    loop

---
+++ Loops.py
@@ -0,0 +1,19 @@
+for i in range(10,1,-3):
+    print(i,end=" ")
+
+i=1
+while i<=5:
+    print("royal")
+    i+=1
+
+
+"""
+1. print all even numbers from 1 to 20.
+2. print all odd numbers from 20 to 30 without if.
+3. Take 5 elements from user and print sum of all elements.
+4. Take input from user and count number of digit. ex. 57567 - 5
+5. Take input from user and print sum of all digits.
+6. Take input from user and print reverse number.
+
+
+"""
